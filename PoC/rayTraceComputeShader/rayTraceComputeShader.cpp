4648f04fa9fec3c8ae70b3198a67db08555e4117
DenisSalem <denissalem@tuxfamily.org>
2016-09-27 17:00:25 +0200

    modifié :         rayTraceComputeShader.cpp 	modifié :         triangle.fs 	modifié :         triangle.vs

--- PoC/rayTraceComputeShader/rayTraceComputeShader.cpp
+++ PoC/rayTraceComputeShader/rayTraceComputeShader.cpp
@@ -22,6 +22,29 @@ typedef struct _RGB_ {
   unsigned char Blue;
 } RGBValues;
 
+void printWorkGroupsCapabilities() {
+  int workgroup_count[3];
+  int workgroup_size[3];
+  int workgroup_invocations;
+
+  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 0, &workgroup_count[0]);
+  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 1, &workgroup_count[1]);
+  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 2, &workgroup_count[2]);
+
+  printf ("Taille maximale des workgroups globaux:\n\tx:%u\n\ty:%u\n\tz:%u\n",
+  workgroup_size[0], workgroup_size[1], workgroup_size[2]);
+
+  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 0, &workgroup_size[0]);
+  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 1, &workgroup_size[1]);
+  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 2, &workgroup_size[2]);
+
+  printf ("Taille maximale des workgroups locaux:\n\tx:%u\n\ty:%u\n\tz:%u\n",
+  workgroup_size[0], workgroup_size[1], workgroup_size[2]);
+
+  glGetIntegerv (GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &workgroup_invocations);
+  printf ("Nombre maximum d'invocation de workgroups locaux:\n\t%u\n", workgroup_invocations);
+}
+
 void compileShader(GLuint shaderID, char * sourcePointer) {
   glShaderSource(shaderID, 1, &sourcePointer , NULL);
   glCompileShader(shaderID);
@@ -121,13 +144,16 @@ int main(int argc, char ** argv) {
   GLuint quadTextureID;
 
   glGenTextures(1, &quadTextureID);
+  glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, quadTextureID);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 640, 480, 0, GL_RGB, GL_UNSIGNED_BYTE, quadTexture);
     glGenerateMipmap(GL_TEXTURE_2D);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glBindImageTexture (0, quadTextureID, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
   glBindTexture(GL_TEXTURE_2D, 0);
-
   
   glGenBuffers(1, &quadIBO);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadIBO);
@@ -149,6 +175,9 @@ int main(int argc, char ** argv) {
       glEnableVertexAttribArray(1);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
+
+  /* ----- Compute Shader ----- */
+  printWorkGroupsCapabilities();
 
   /* ----- Vertex shaders and Fragments shaders ----- */
 
--- PoC/rayTraceComputeShader/triangle.vs
+++ PoC/rayTraceComputeShader/triangle.vs
@@ -1,4 +1,4 @@
-#version 440
+#version 430
 
 in vec3 vertex;
 layout(location = 0) in vec2 position;
